077ee8db35bf6801a9360822f0b7d3c5a1883df9
amishvartech-creator <amish.vartech@gmail.com>
2026-08-15 16:36:29 +0530

    Add files via upload

---
+++ variables_demo.py
@@ -0,0 +1,18 @@
+# Declare variables
+followers = 12500
+average_rating = 4.7
+favorite_app = "Instagram"
+is_premium_user = True
+
+# Print each variable and its type
+
+print("followers:", followers)
+print("average_rating:", average_rating)
+print("favorite_app:", favorite_app)
+print("is_premium_user:", is_premium_user)
+print(type(followers))
+print(type(average_rating))
+print(type(favorite_app))
+print(type(is_premium_user))
+
+
