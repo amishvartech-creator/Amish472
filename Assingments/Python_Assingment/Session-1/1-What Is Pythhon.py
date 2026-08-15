a501269909a1a909d8d9fe5054b19b10ba2b0b02
amishvartech-creator <amish.vartech@gmail.com>
2026-08-15 14:54:18 +0530

    Add files via upload

---
+++ Assingments/Python_Assingment/Session-1/1-What Is Pythhon.py
@@ -0,0 +1,31 @@
+"""
+What is Python?
+
+1. python is server side scripting language.
+
+2. python interprete (step by step check programme and convert high level language into    
+   binary level) based language.
+ 
+3. python saved file with .py extension
+
+4. python is very old language that can be used to create... 
+
+- windows applications 
+- web site apps 
+- machine learning 
+- data analytics 
+- business analytics 
+- data science 
+- iot 
+- game development 
+- Ai/ML
+
+5 python used print() function to print data
+
+"""
+
+Installation of python:
+
+pip install notebook
+
+jupyter notebook
---
+++ Assingments/Python_Assingment/Session-1/hello_app.py
@@ -0,0 +1,3 @@
+# Welcome to Data Analysis with Python! 
+# This application is designed to help you explore and analyze data using various Python libraries. 
+# You can load datasets, perform statistical analysis, visualize data, and gain insights from your data. Let's get started!
